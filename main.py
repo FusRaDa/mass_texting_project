1e024110f0228bccf3cf2e052a0e685945ba5be7
Matthew Rada <matthewr7.christopher@gmail.com>
2023-05-04 08:58:49 -0400

    excel working

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import time
 
+import openpyxl
 import pywinauto.application
 from pywinauto.application import Application
 
@@ -7,12 +8,41 @@ from pywinauto.application import Application
 
 # grasshopper desktop app must be launched and logged in
 
-
-def get_number_and_name():
-    print('hi')
+excel_file_path = 'C:\\Users\\FusRada\\Desktop\\test_sheet.xlsx'
 
 
-def text_phone_number(application):
+def get_list_of_number_and_name():
+    wb = openpyxl.load_workbook(excel_file_path)
+    sheet = wb.active
+
+    column_a = sheet['A']
+    column_b = sheet['B']
+
+    phone_number_array = []
+    first_name_array = []
+
+    for x in range(1, len(column_a)):
+        phone_number_array.append(column_a[x].value)
+
+    for x in range(1, len(column_b)):
+        first_name_array.append(column_b[x].value)
+
+    full_list = []
+
+    if len(phone_number_array) == len(first_name_array):
+        for x in range(len(first_name_array)):
+            number_and_name = {'number': phone_number_array[x], 'name': first_name_array[x]}
+            full_list.append(number_and_name)
+    else:
+        print("Error check the excel file. All columns must be the same length")
+
+    print(full_list)
+    return full_list
+
+
+def text_phone_number(application, number, name):
+
+    message = "Hi there " + name + "!"
 
     send_message = application['Grasshopper App'].child_window(title="Send a Message",
                                                                control_type="DataItem").wrapper_object()
@@ -21,7 +51,7 @@ def text_phone_number(application):
     input_number = application['Grasshopper App'].child_window(title="Type a phone number", auto_id="sms-dialed-num",
                                                                control_type="Edit").wrapper_object()
     input_number.click_input()
-    input_number.type_keys("2402102023")
+    input_number.type_keys(number)
 
     emoji = application['Grasshopper App'].child_window(title="Emoji Picker", control_type="Image").wrapper_object()
     emoji.click_input()
@@ -29,27 +59,30 @@ def text_phone_number(application):
     input_message = application['Grasshopper App'].child_window(title="Type a message",
                                                                 control_type="Edit").wrapper_object()
     input_message.click_input()
-    input_message.type_keys("Hi there {ENTER}", with_spaces=True)
+    input_message.type_keys(message + "{ENTER}", with_spaces=True)
 
     time.sleep(2)
 
 
-try:
-    # start up
-    app = Application(backend='uia').connect(title='Grasshopper App')
-    # app['Grasshopper App'].print_control_identifiers()
-    messages_tab = app['Grasshopper App'].child_window(auto_id="messages", control_type="Custom").wrapper_object()
-    messages_tab.click_input()
+def begin_mass_texting(dict_list):
 
-    for i in range(0, 3):
-        text_phone_number(app)
+    try:
+        # start up
+        app = Application(backend='uia').connect(title='Grasshopper App')
+        # app['Grasshopper App'].print_control_identifiers()
+        messages_tab = app['Grasshopper App'].child_window(auto_id="messages", control_type="Custom").wrapper_object()
+        messages_tab.click_input()
+
+        for i in range(len(dict_list)):
+            text_phone_number(app, dict_list[i]['number'], dict_list[i]['name'])
+
+    except pywinauto.application.ProcessNotFoundError:
+        print("failed to connect to app, grasshopper desktop app must be launched and logged in")
 
 
+begin_mass_texting(get_list_of_number_and_name())
 
 
-
-except pywinauto.application.ProcessNotFoundError:
-    print("failed to connect to app, grasshopper desktop app must be launched and logged in")
 
 
 
